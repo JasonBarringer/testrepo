08cfed5f73b2b4f8da0e29dff6cccb07488df68f
JasonBarringer <jbarring@uw.edu>
2020-01-16 20:44:51 -0800

    New Commit

---
+++ AsciiArt.java
@@ -0,0 +1,12 @@
+public class AsciiArt{
+    public static void main(String[] args){
+        System.out.println("   _________");
+        System.out.println("  /         \\");
+        System.out.println("  |         |");
+        System.out.println("__|_________|__");
+        System.out.println(" (''''(o)^(0)|");
+        System.out.println("  |  _   _\\ (");
+        System.out.println("  \\   |  __ /");
+        System.out.println("   \\_______/");
+    }
+}
---
+++ hmmm/guy.java
@@ -0,0 +1,13 @@
+package hmmm;
+
+public class guy{
+    int testinteger;
+
+    public guy(int testinteger){
+        this.testinteger = testinteger;
+    }
+
+    public void update(){
+        this.testinteger += 10;
+    }
+}
