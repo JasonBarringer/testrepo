2be982ed6f648d868935da7874399f703347885a
JasonBarringer <jbarring@uw.edu>
2020-01-29 12:18:30 -0800

    jjjj

--- Grades.java
+++ Grades.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Grades {
     public static void main(String[] args) {
         int a1 = 17;
@@ -7,14 +9,16 @@ public class Grades {
         int a2p = 20;
         int a3p = 20;
 
-        int midterm = 88;
-        int fin = 82;
+        int midterm = 95;
+        int fin = 98;
 
         double assignAvg = calculateAverage(a1, a2, a3, a1p, a2p, a3p);
 
         double overall = calculateGrade(assignAvg, midterm, fin);
 
         printResults(assignAvg, midterm, fin, overall);
+        printAchievements(assignAvg, midterm, fin, overall);
+        readassignscores();
     }
 
     public static double calculateAverage(int a1, int a2, int a3, int a1p, int a2p, int a3p){
@@ -33,5 +37,30 @@ public class Grades {
         System.out.println("Midterm: " + midterm);
         System.out.println("Final: " + fin);
         System.out.println("Overall grade: " + overall);
+    }
+
+    public static void printAchievements(double assigns, int mid, int fin, double overall) {
+        if(assigns >= .90) {
+            System.out.println("like so brody can see");
+        }
+        if(fin > mid) {
+            System.out.println("I'm straight miffed rn bro omg");
+        } else {
+            System.out.println("bro you kinda suck tbh be onest");
+        }
+        int heyya = 0;
+        //do
+        do {
+            System.out.println("alright");
+            heyya += 1;
+        } while (heyya <= 10);
+
+        System.out.println("this is kinda cring ngl");
+    }
+
+    public static int readassignscores(){
+        Scanner console = new Scanner(System.in);
+        int score = console.nextInt();
+        return(score);
     }
 }
