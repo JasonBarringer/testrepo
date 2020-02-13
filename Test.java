96307c34677cdf292db2cc9dd2d28325648f23c3
JasonBarringer <jbarring@uw.edu>
2020-02-13 11:18:40 -0800

    quiz section 2/13/2020

--- Test.java
+++ Test.java
@@ -2,24 +2,27 @@ import java.util.*;
 
 public class Test{
     public static void main(String[] args){
-        randomWalk();
+        Random r = new Random();
+        spinWheel(r, 3);
     }
 
-    public static void randomWalk() {
-        Random r = new Random();
-        int x = 0;
-        int rr = 0;
-        int max = 0;
-        int length = 0;
-        while(Math.abs(x) < 3) {
-            length += 1;
-            System.out.println("position = " + x);
-            rr = r.nextInt(3) - 1;
-            x += rr;
-            max = Math.max(x, max);
-        }
-        System.out.println("position = " + x);
-        System.out.println("max position = " + max);
-        System.out.println("length = " + length);
+    public static void spinWheel(Random r, int n) {
+        int x = (r.nextInt(5) + 2) * 10;
+        int count = 0;
+        int count2 = 0;
+        System.out.println("spins: ");
+        do {
+            System.out.print(x + ", ");
+            x = (r.nextInt(5) + 2) * 10;
+            if(20 == x) {
+                count2 += 1;
+            } else {
+                count2 = 0;
+            }
+            count += 1;
+        } while(count2 != n);
+        System.out.println(x);
+
+        System.out.println(n + " in a row after " + count + " spins");
     }
 }
