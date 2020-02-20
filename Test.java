40352c76335f0c54aeafee6bd9974d89be878d17
JasonBarringer <jbarring@uw.edu>
2020-02-20 11:18:14 -0800

    test stuff

--- Test.java
+++ Test.java
@@ -1,28 +1,24 @@
 import java.util.*;
+import java.io.*;
 
 public class Test{
-    public static void main(String[] args){
-        Random r = new Random();
-        spinWheel(r, 3);
-    }
+    public static void main(String[] args) throws FileNotFoundException{
+        Scanner input = new Scanner(new File("testFile.txt"));
+        while(input.hasNextLine()){
+            String line = input.nextLine();
+            Scanner lineScan = new Scanner(line);
+            while (lineScan.hasNext()) {
+                String word = lineScan.next();
+                word.replace("o", "0");
+                word.replace("e", "3");
+                word.replace("a", "4");
+                word.replace("t", "7");
+                if(word.endsWith("s")) {
+                    word.replace("s", "z");
+                }
 
-    public static void spinWheel(Random r, int n) {
-        int x = (r.nextInt(5) + 2) * 10;
-        int count = 0;
-        int count2 = 0;
-        System.out.println("spins: ");
-        do {
-            System.out.print(x + ", ");
-            x = (r.nextInt(5) + 2) * 10;
-            if(20 == x) {
-                count2 += 1;
-            } else {
-                count2 = 0;
             }
-            count += 1;
-        } while(count2 != n);
-        System.out.println(x);
-
-        System.out.println(n + " in a row after " + count + " spins");
+            System.out.println();
+        }
     }
 }
